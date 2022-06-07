279cef5853b6672a19b719753e825c0ba11bb932
SB <shreyas21@vt.edu>
2022-06-07 15:15:44 -0400

    More updates to the create changes in the MDP class
    Will work more on it in a bit. current type errors happening that need to be fixed.

--- Network_Attack_Simulator/nasim/scenarios/benchmark/yaml_modifications.py
+++ Network_Attack_Simulator/nasim/scenarios/benchmark/yaml_modifications.py
@@ -19,27 +19,65 @@ class YAML_mod:
         param_type = self.param_type
         safe = ""
 
-        if param_type == 1:
-            pass
+        """   if param_type == 1:
+            all_files = {filename: open(filename, "r") for filename in self.filename}
+            safe = str(all_files)
+        """
 
         if param_type == 2:
             with open(filename, 'r') as file:
                 safe = yaml.safe_load(file)
         
+        self.safe = safe
         return safe
+
+    def get_parts(self):
+        """
+            This function will get the specirfic parts for what is 
+            needed for the MDP of the chosen yaml file
+
+            - firewall -> P
+            - subnet and topology -> S 
+            - explits and privilege escalation -> A
+
+            Are the main parts of the yaml needed to then make changes
+            to the MDP/ network model as needed. 
+        """
+        safe = self.safe
+
+        firewall = safe["firewall"]
+
+        subnets = safe["subnets"]
+        topology = safe["topology"]
+        sensitive = safe["sensitive_hosts"]
+
+        exploits = safe["exploits"]
+        escalation = safe["privilege_escalation"]
+
+
+        self.states = firewall 
+
+        self.probability = subnets, topology, sensitive, sensitive 
+
+        self.actions = exploits,escalation
+
+        return self.states, self.probability, self.actions
+
+
+
+
 
 
     def __str__(self):
         return self.filename
 
     
-yaml_list = ["tiny.yaml", "small.yaml"]
+yaml_list = "small.yaml"
 
 
 read_yaml = YAML_mod(yaml_list)
-print(read_yaml.what_type())
 read_yaml = read_yaml.read_file()
-print(read_yaml)
+read_yaml = read_yaml.get_parts()
 
 
 
