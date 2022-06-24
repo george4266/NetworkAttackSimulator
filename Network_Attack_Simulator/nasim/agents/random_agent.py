f58866690526017d612b808e743c2b50f4087e2f
SB <shreyas21@vt.edu>
2022-06-24 18:34:47 -0400

    evening updates 6.24.22

--- Network_Attack_Simulator/nasim/agents/random_agent.py
+++ Network_Attack_Simulator/nasim/agents/random_agent.py
@@ -23,6 +23,11 @@ command line output
 
 import numpy as np
 import pandas as pd
+from rich import print as rprint
+from rich.panel import Panel
+
+import plotly.express as px
+
 
 import nasim
 
@@ -52,7 +57,7 @@ def run_random_agent(env, step_limit=1e6, verbose=True):
         t += 1
 
         count+=1
-        df.loc[len(df.index)] = [t, total_reward, done, x, _]
+        df.loc[len(df.index)] = [t, total_reward, done, x, _, a]
 
     if done and verbose:
         print(LINE_BREAK)
@@ -76,8 +81,9 @@ def run_random_agent(env, step_limit=1e6, verbose=True):
 
 
 if __name__ == "__main__":
+    rprint("[red]Starting...[/red]")
 
-    df = pd.DataFrame(columns=['steps','rewards', 'done', 'topology?','agent knows?'])
+    df = pd.DataFrame(columns=['steps','rewards', 'done', 'topology','_x_', "action_num_val"])
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("env_name", type=str,
@@ -104,6 +110,8 @@ if __name__ == "__main__":
                                    not args.partially_obs,
                                    not args.param_actions,
                                    not args.box_obs)
+
+        #appending data                       
         steps, reward, done, count = run_random_agent(env, verbose=False)
         
         run_steps.append(steps)
@@ -116,14 +124,50 @@ if __name__ == "__main__":
             print(f"\tSteps = {steps}")
             print(f"\tReward = {reward}")
             print(f"\tGoal reached = {done}")
+            print(i)
     print(LINE_BREAK)
     print("Number of runs",count)
 
     
     df.to_csv("random_out.csv")
+    rprint("[yellow]DataFrame Created![/yellow]")
+    rprint("DATAFRAME"+"\n",df)
+
+
+    """
+    Finds the amount of unique counts from the topology 
+    column of df
+    """
+    df["topology"] = df["topology"].astype("string")
+    dups = df.pivot_table(columns=["topology"],aggfunc="size")
+    rprint(dups) 
+
+    """
+    For some reason this isn't becoming a proper csv :P
+    """
+    topology_dups = pd.DataFrame(columns=["Topology"])
+    topology_dups.loc[len(topology_dups)] = [dups]
+    topology_dups.to_csv("random_out2.csv")
+
+    rprint("[yellow]Topology Duplicates[/yellow]")
+    rprint(topology_dups)
+
 
     run_steps = np.array(run_steps)
     run_rewards = np.array(run_rewards)
+
+    """
+    This DataFrame is useless for right now. Therefore it
+    is just printed to the console 
+    """
+    rprint("")
+    df2 = pd.DataFrame(columns=["steps", "rewards"])
+    df2.loc[len(df2.index)] = [run_steps, run_rewards]
+    rprint(df2)
+    #----------------
+    
+    rprint("[green]Completed![/green]")
+
 
     print(LINE_BREAK)
     print("Random Agent Runs Complete")
@@ -134,3 +178,11 @@ if __name__ == "__main__":
     print(f"Goals reached = {run_goals} / {args.runs}")
 
 
+
+    """
+    Display the steps vs. rewards from df
+    """
+    fig = px.line(df, x="steps", y="rewards")
+
+    fig.update_layout(hovermode ="x")
+    fig.show()
